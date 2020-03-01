c2c6bb84b83bc1464086e8bd9929075a6b68b9ba
Myroslav Oleinykov <m@meeroslaph.name>
2020-03-01 17:48:16 +0200

    Insurance start date tests

--- src/test/java/pages/SelectPreconditionPage.java
+++ src/test/java/pages/SelectPreconditionPage.java
@@ -1,10 +1,13 @@
 package pages;
 
 import org.openqa.selenium.By;
+import static com.codeborne.selenide.Selenide.$;
 
 public class SelectPreconditionPage extends BasePage {
     private static final String URL_ENDING = "selectPrecondition";
     private static final By PAGE_LOCATOR = By.xpath("//span[text()='In 90 Sekunden zum Beitrag']");
+
+    private final By invalidDateError = By.cssSelector("div[class^='ValidationLabel']");
 
     public SelectPreconditionPage() {
         super(URL_ENDING, PAGE_LOCATOR);
@@ -15,4 +18,20 @@ public class SelectPreconditionPage extends BasePage {
         return new SelectRegisteredOwnerPage();
     }
 
+    public SelectPreconditionPage enterInsuranceStartDate(String date) {
+        $("input[name='inceptionDate']").setValue(date);
+        return this;
+    }
+
+    public boolean isInvalidDateErrorMessageDisplayed() {
+        return $(invalidDateError).isDisplayed();
+    }
+
+    public boolean isSubmitButtonEnabled() {
+        return new CommonElements().isSubmitButtonActive();
+    }
+
+    public String getInvalidDateErrorMessage() {
+        return $(invalidDateError).text();
+    }
 }
