c01aa8a076f8f19b1402735e532b1b0908e8eaf6
Myroslav Oleinykov <m@meeroslaph.name>
2020-03-02 14:05:47 +0200

    Adding possibility to run tests either locally, using CI remote server, or 3rd party provider SauceLabs

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,25 +1,56 @@
 package tests;
 
-import com.codeborne.selenide.Browsers;
-import com.codeborne.selenide.Configuration;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import pages.SelectPreconditionPage;
 import static com.codeborne.selenide.Configuration.baseUrl;
+import static com.codeborne.selenide.Configuration.browser;
+import static com.codeborne.selenide.Configuration.browserCapabilities;
+import static com.codeborne.selenide.Configuration.remote;
+import static com.codeborne.selenide.Configuration.startMaximized;
+import static com.codeborne.selenide.Configuration.timeout;
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.Selenide.open;
 
 abstract class BaseTest {
     SelectPreconditionPage selectPreconditionPage;
 
-    @BeforeMethod
+    @BeforeSuite
     public void setUp() {
-        Configuration.baseUrl = "https://hello.friday.de";
-        Configuration.timeout = 10000;
-        Configuration.startMaximized = true;
-        Configuration.browser = Browsers.CHROME;
-        Configuration.remote = "http://localhost:4444/wd/hub";
+        baseUrl = Config.BASE_URL;
+        browser = Config.BROWSER;
+        startMaximized = Boolean.parseBoolean(Config.START_MAXIMIZED);
+        timeout = Long.parseLong(Config.TIMEOUT);
 
+        switch (Config.ENV) {
+            case "local":
+                break;
+            case "remote":
+                remote = Config.REMOTE;
+                break;
+            case "saucelabs":
+                String browserVersion = Config.BROWSER_VERSION;
+                String platform = Config.PLATFORM;
+
+                DesiredCapabilities capabilities = new DesiredCapabilities();
+                capabilities.setCapability("browserName", browser);
+                capabilities.setCapability("browserVersion", browserVersion);
+                capabilities.setCapability("platformName", platform);
+                capabilities.setCapability("name", "Test");
+
+                remote = String.format("http://%s:%s@ondemand.saucelabs.com:80/wd/hub",
+                    Config.SAUCE_USER, Config.SAUCE_KEY);
+                browserCapabilities = capabilities;
+                break;
+            default:
+                throw new IllegalStateException("Environment system property '" + Config.ENV + "' is not supported");
+        }
+    }
+
+    @BeforeMethod
+    public void openHomePage() {
         open(baseUrl);
         selectPreconditionPage = new SelectPreconditionPage();
     }
